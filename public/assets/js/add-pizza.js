579edb528d9a3bf4dde224b217b4ada64dbc50c9
Erica <erica.trenholm48@gmail.com>
2022-05-15 13:59:15 -0400

    added indexedDB

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -60,14 +60,14 @@ const handlePizzaSubmit = event => {
     },
     body: JSON.stringify(formData)
   })
-  .then(response => response.json())
-  .then(postResponse => {
-    alert('Pizza created successfully!');
-    console.log(postResponse)
-  })
-  .catch(err => {
-    console.log(err)
-  })
+    .then(response => response.json())
+    .then(postResponse => {
+      console.log(postResponse);
+    })
+    .catch(err => {
+      console.log(err);
+      saveRecord(formData);
+    });
 };
 
 $pizzaForm.addEventListener('submit', handlePizzaSubmit);
